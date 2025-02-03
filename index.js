a6af99be541e511a907e59f1a3b4eaf7a0c84791
nikolayfedyunin <forfedyunin@gmail.com>
2025-02-03 23:27:42 +0300

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log(1);
+
+const button = document.querySelector('button');
+const output = document.querySelector('.output');
+
+button.addEventListener('click', function() {
+    output.innerHTML = 'The extraterrestrial wonders are kept here 👽🛸';
+});
